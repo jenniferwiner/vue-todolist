1634dfdc8ab912bbc9c7fe9866fa5f12ad97673d
jenniferwiner <jeniwiner@gmail.com>
2017-11-05 18:10:37 -0700

    List component works to delete and add todolist items

---
+++ src/components/TodoList.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <input
+      v-model="newTodoText"
+      v-on:keyup.enter="addNewTodo"
+      placeholder="Add a todo"
+    >
+    <div>
+      <todo
+        v-for="(todo, index) in todos"
+        v-bind:key="todo.id"
+        v-bind:title="todo.title"
+        v-on:delete="todos.splice(index, 1)"
+      ></todo>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+  import Todo from './ListItem'
+
+  export default {
+    data() {
+      return {
+        newTodoText: '',
+        todos: [
+          {
+            id: 1,
+            title: 'Do the dishes',
+          },
+          {
+            id: 2,
+            title: 'Take out the trash',
+          },
+          {
+            id: 3,
+            title: 'Mow the lawn'
+          }
+        ],
+        nextTodoId: 4
+      }
+    },
+    methods: {
+      addNewTodo: function () {
+        this.todos.push({
+          id: this.nextTodoId++,
+          title: this.newTodoText
+        })
+        this.newTodoText = ''
+      }
+    },
+    components: {
+      Todo,
+    }
+  }
+</script>
+<style>
+</style>
